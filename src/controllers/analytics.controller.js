84b5119651b1f8b346d4bdff441009fa77c28027
SaiKriCius <jatin6410kumar@gmail.com>
2026-04-12 22:10:59 +0530

    feat: implement analytics controller for retrieving total and daily sales metrics

--- src/controllers/analytics.controller.js
+++ src/controllers/analytics.controller.js
@@ -5,7 +5,7 @@ import User from "../models/user.model.js";
 export const getAnalyticsData = async () => {
 	const totalUsers = await User.countDocuments();
 	const totalProducts = await Product.countDocuments();
-
+//checking
 	const salesData = await Order.aggregate([
 		{
 			$group: {
